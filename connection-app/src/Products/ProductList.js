c13c0a16bdd0db51af7259323d8aeccff3623908
dkrmentor <dkrmentor@gmail.com>
2023-02-03 13:18:00 +0500

    connection

--- connection-app/src/Products/ProductList.js
+++ connection-app/src/Products/ProductList.js
@@ -1,17 +1,53 @@
 import React from 'react'
 
-const ProductList = ({products}) => {
+
+const ProductList = ({ products }) => {
+  // const prod = products.map((product) => {
+  //   return {
+  //     content: `${product.title} - ${product.description}`,
+  //     price: `${product.variants[0].price.amount}`,
+  //     img: `${product.images[0].src}`,
+  //   }
+  // })
+  // console.log(prod)
+
+  const prods = products.map((product) => {
+    const content = `${product.title} - ${product.description}`
+    const price = `${product.variants[0].price.amount}`
+    const img = `${product.images[0].src}`
+    return (
+      <li>{ }</li>
+      
+    )
+
+
+  })
+  console.log(prods)
+
   return (
-    <div>
-    {products.map((product) => (
-      <div key={product.id}>
-        <p>{JSON.stringify(product)}</p>
-        {/* <h2>{product.title}</h2>
-        <img src={product.images[0].src} alt={product.title} />
-        <p>{product.description}</p> */}
+    <>
+      {/* <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <p>{JSON.stringify(product)}</p>
+            <h2>{product.title}</h2>
+            <img src={product.images[0].src} alt={product.title} />
+            <p>{product.description}</p>
+            <p>{product.variants[0].price.amount}</p>
+          </div>
+        ))}
+      </div> */}
+
+      {/* <div>
+        <p>{JSON.stringify(prod)}</p>
+      </div> */}
+
+      <div>
+
       </div>
-    ))}
-  </div>
+    </>
+
+
   )
 }
 
